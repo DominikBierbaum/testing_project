ac2f1c3f4679ac09ab2edd928630fbf9160aa784
Pietro Berkes <pietro.berkes@googlemail.com>
2023-08-14 22:50:44 +0200

    Complete the content of the testing project repository

--- test_logistic.py
+++ test_logistic.py
@@ -2,17 +2,7 @@ from numpy.testing import assert_allclose
 
 from logistic import f
 
-
-def test_f():
-    # Test cases are (x, r, expected)
-    cases = [
-        (0.1, 2.2, 0.198),
-        (0.2, 3.4, 0.544),
-        (0.5, 2, 0.5),
-    ]
-    for x, r, expected in cases:
-        result = f(x, r)
-        assert_allclose(result, expected)
+# Add here your test for the logistic map
 
 
 def test_f_corner_cases():
