5b09ac7784a98d27e47ec000970a66000c4c877f
ASPP Student <student@localhost>
2023-08-29 12:11:11 +0300

    logistic function and test

--- logistic.py
+++ logistic.py
@@ -1,1 +1,4 @@
 # Your code goes here
+def f(x, r):
+    return r * x * (1 - x)
+    
--- test_logistic.py
+++ test_logistic.py
@@ -3,6 +3,12 @@ from numpy.testing import assert_allclose
 from logistic import f
 
 # Add here your test for the logistic map
+def test_f_generic():
+    x = [0.1, 0.2, 0.5]
+    r = [2.2, 3.4, 2]
+    expected_result = [0.198, 0.544, 0.5]
+    for i in range(len(x)):
+        assert_allclose(f(x[i], r[i]), expected_result[i])
 
 
 def test_f_corner_cases():
